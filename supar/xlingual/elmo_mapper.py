c0d8e4c787db4511fe04344754b9853a85771bd1
MatejUlcar <matej.ulcar@protonmail.com>
2020-11-10 13:10:47 +0000

    xlingual list formatting fix

--- supar/xlingual/elmo_mapper.py
+++ supar/xlingual/elmo_mapper.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import Model, load_model
 from supar.xlingual.apply_vecmap_transform import vecmap
 import numpy as np
 
+
 class Elmogan():
     def __init__(self, args):
         self.layer0 = load_model(args.map_layer0)
@@ -13,9 +14,9 @@ class Elmogan():
     def map_batch(self, batch):
         for x,sentence in enumerate(batch):
             seqlen = sentence[0].shape[0]
-            batch[x, 0, 0:seqlen, :] = apply_mapping(sentence[0], self.layer0)
-            batch[x, 1, 0:seqlen, :] = apply_mapping(sentence[1], self.layer1)
-            batch[x, 2, 0:seqlen, :] = apply_mapping(sentence[2], self.layer2)
+            batch[x, 0, 0:seqlen] = self.apply_mapping(sentence[0], self.layer0)
+            batch[x, 1, 0:seqlen] = self.apply_mapping(sentence[1], self.layer1)
+            batch[x, 2, 0:seqlen] = self.apply_mapping(sentence[2], self.layer2)
         return batch
             
     def apply_mapping(self, sentence, W):
@@ -36,21 +37,20 @@ class Vecmap():
         self.layer0 = np.load(args.map_layer0)
         self.layer1 = np.load(args.map_layer1)
         self.layer2 = np.load(args.map_layer2)
-        #self.direction = args.map_direction
         if args.vecmap_lang in ('source', 'src'):
             self.lang = 'wx2'
         elif args.vecmap_lang in ('target', 'trg', 'tgt'):
             self.lang = 'wz2'
         else:
             self.lang = None
-        
+    
     def map_batch(self, batch):    
         for x,sentence in enumerate(batch):
             seqlen = sentence[0].shape[0]
-            batch[x, 0, 0:seqlen, :] = apply_mapping(sentence[0], self.layer0)
-            batch[x, 1, 0:seqlen, :] = apply_mapping(sentence[1], self.layer1)
-            batch[x, 0:seqlen, :] = apply_mapping(sentence[2], self.layer2)
-        return emb0, emb1, emb2
+            batch[x][0][0:seqlen] = self.apply_mapping(sentence[0], self.layer0)
+            batch[x][1][0:seqlen] = self.apply_mapping(sentence[1], self.layer1)
+            batch[x][2][0:seqlen] = self.apply_mapping(sentence[2], self.layer2)
+        return batch
     
     def apply_mapping(self, sentence, W):
         if W:
